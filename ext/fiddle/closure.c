b7c0e539c0c028d32ced8cea451396f4c9f2ed9b
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-04-05 19:17:30 +0900

    Added workround for `ruby_thread_has_gvl_p` provided by internal.h.

--- ext/fiddle/closure.c
+++ ext/fiddle/closure.c
@@ -1,6 +1,7 @@
 #include <fiddle.h>
 #include <ruby/thread.h>
-#include "internal.h" /* rb_thread_has_gvl_p */
+
+int ruby_thread_has_gvl_p(void); /* for ext/fiddle/closure.c */
 
 VALUE cFiddleClosure;
 
