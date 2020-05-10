6a9a8171aed208b87daf48e2954908985d239072
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-05-10 14:58:55 +0900

    extlibs.rb: added variable references
    Reduce duplicate parts such as package name and version numbers.
    
    ruby/ruby@d1748484e8

--- bin/extlibs.rb
+++ bin/extlibs.rb
@@ -6,6 +6,20 @@
 require 'digest'
 require_relative 'downloader'
 
+class Vars < Hash
+  def pattern
+    /\$\((#{Regexp.union(keys)})\)/
+  end
+
+  def expand(str)
+    if empty?
+      str
+    else
+      str.gsub(pattern) {self[$1]}
+    end
+  end
+end
+
 class ExtLibs
   def cache_file(url, cache_dir)
     Downloader.cache_file(url, nil, :cache_dir => cache_dir)
@@ -142,16 +156,21 @@ class ExtLibs
           $stdout.puts "downloading for #{list}"
           $stdout.flush
         end
+        vars = Vars.new
         extracted = false
         dest = File.dirname(list)
         url = chksums = nil
         IO.foreach(list) do |line|
           line.sub!(/\s*#.*/, '')
+          if /^(\w+)\s*=\s*(.*)/ =~ line
+            vars[$1] = vars.expand($2)
+            next
+          end
           if chksums
             chksums.concat(line.split)
           elsif /^\t/ =~ line
             if extracted and (mode == :all or mode == :patch)
-              patch, *args = line.split
+              patch, *args = line.split.map {|s| vars.expand(s)}
               do_patch(dest, patch, args)
             end
             next
@@ -162,7 +181,11 @@ class ExtLibs
             chksums.pop
             next
           end
-          next unless url
+          unless url
+            chksums = nil
+            next
+          end
+          url = vars.expand(url)
           begin
             extracted = do_command(mode, dest, url, cache_dir, chksums)
           rescue => e
