49f359343e56d9abda4f5aee4ab6139630ebcbe7
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-09-05 17:11:43 +0900

    Remove extraneous quotes
    `MakeMakefile#create_header` does not expect each entire option in
    `$defs` to be enclosed in quotation marks.

--- ext/-test-/auto_ext.rb
+++ ext/-test-/auto_ext.rb
@@ -5,7 +5,7 @@ def auto_ext(feat = $0[%r[/ext/(-test-/.*)/extconf.rb\z], 1], inc: false)
   inits = $srcs.map {|s| File.basename(s, ".*")}
   inits.delete("init")
   inits.map! {|s|"X(#{s})"}
-  $defs << "'-DTEST_INIT_FUNCS(X)=\"#{inits.join(' ')}\"'"
+  $defs << "-DTEST_INIT_FUNCS(X)=\"#{inits.join(' ')}\""
   create_header
   create_makefile(feat)
 end
