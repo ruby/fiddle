910f48e1783baa67ae760c5a36f22062e5e1323d
Sutou Kouhei <kou@clear-code.com>
2024-10-07 11:03:10 +0900

    Import JRuby implementation (#147)
    Fix GH-104
    
    lib/fiddle/jruby.rb is based on
    
    https://github.com/jruby/jruby/blob/master/lib/ruby/stdlib/fiddle/jruby.rb
    .
    
    Here are changes for it:
    
    * Move `Fiddle::TYPE_*` to `Fiddle::Types::*`
    * Add `Fiddle::Types::VARIADIC`
    * Add `Fiddle::Types::CONST_STRING`
    * Add `Fiddle::Types::BOOL`
    * Add `Fiddle::Types::INT8_T`
    * Add `Fiddle::Types::UINT8_T`
    * Add `Fiddle::Types::INT16_T`
    * Add `Fiddle::Types::UINT16_T`
    * Add `Fiddle::Types::INT32_T`
    * Add `Fiddle::Types::UINT32_T`
    * Add `Fiddle::Types::INT64_T`
    * Add `Fiddle::Types::UINT64_T`
    * Add more `Fiddle::ALIGN_*` for the above new `Fiddle::Types::*`
    * Add more `Fiddle::SIZEOF_*` for the above new `Fiddle::Types::*`
    * Add support for specifying type as not only `Fiddle::Types::*` but
    also `Symbol` like `:int`
    * Add support for variable size arguments in `Fiddle::Function`
    * Add `Fiddle::Closure#free`
    * Add `Fiddle::Closure#freed?`
    * Add `Fiddle::Error` as base the error class
    * Add `Fiddle::Pointer#call_free` and stop using `FFI::AutoPointer` in
    `Fiddle::Pointer`
    * Add support for `Fiddle::Pointer.malloc {}` `Fiddle::Pointer`
    * Add support for `Fiddle::Pointer#free=`
    * Add `Fiddle::Pointer#freed?`
    * Use binary string not C string for `Fiddle::Pointer#[]`
    * Add `Fiddle::Handle.sym_defined?`
    * Add `Fiddle::Handle#sym_defined?`
    * Add `Fiddle::Handle::DEFAULT`
    * Add `Fiddle::ClearedReferenceError`
    * Add no-op `Fiddle::Pinned`
    
    Some features are still "not implemented". So there are some "omit"s for
    JRuby in tests.

--- test/run.rb
+++ test/run.rb
@@ -3,14 +3,19 @@
 $VERBOSE = true
 
 source_dir = "#{__dir__}/.."
-$LOAD_PATH.unshift("#{source_dir}/test")
-$LOAD_PATH.unshift("#{source_dir}/lib")
+if File.exist?("#{source_dir}/lib")
+  # Test against Fiddle in source directory
+  $LOAD_PATH.unshift("#{source_dir}/lib")
 
-build_dir = Dir.pwd
-if File.exist?("#{build_dir}/fiddle.so")
-  $LOAD_PATH.unshift(build_dir)
+  build_dir = Dir.pwd
+  if File.exist?("#{build_dir}/fiddle.so")
+    $LOAD_PATH.unshift(build_dir)
+  end
+else
+  # Test against Fiddle installed as gem
+  gem "fiddle"
 end
 
-Dir.glob("#{source_dir}/test/fiddle/test_*.rb") do |test_rb|
-  require File.expand_path(test_rb)
-end
+require "test/unit"
+
+exit Test::Unit::AutoRunner.run(true, "#{source_dir}/test/fiddle")
