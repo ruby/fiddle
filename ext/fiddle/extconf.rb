ebac921a746de311fba53117a35840e757529706
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-10-22 18:15:00 +0900

    Suppress -Wundef warnings
    The GCC warning puts undefined macro names in double quotes while
    clang puts them in single quotes.

--- ext/fiddle/extconf.rb
+++ ext/fiddle/extconf.rb
@@ -69,7 +69,7 @@ unless bundle
   if have_ffi_header && (have_library('ffi') || have_library('libffi'))
     have_libffi = true
     checking_for("undefined FFI_GO_CLOSURES is used") do
-      if egrep_cpp(/warning: 'FFI_GO_CLOSURES' is not defined/, cpp_include(ffi_header), "2>&1")
+      if egrep_cpp(/warning: \WFFI_GO_CLOSURES\W is not defined/, cpp_include(ffi_header), "2>&1")
         $defs.push('-DFFI_GO_CLOSURES=0')
       end
     end
