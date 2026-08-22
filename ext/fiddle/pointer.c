824f063e8e11b3ac7c4b3ec5e81af2d02b28c8f6
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-22 15:19:38 +0900

    Make Pointer MemoryView accept WRITABLE flag

--- ext/fiddle/pointer.c
+++ ext/fiddle/pointer.c
@@ -137,7 +137,7 @@ fiddle_ptr_memory_view_available_p(VALUE obj)
 static bool
 fiddle_ptr_get_memory_view(VALUE obj, rb_memory_view_t *view, int flags)
 {
-    if (flags != RUBY_MEMORY_VIEW_SIMPLE) return false;
+    if (flags != RUBY_MEMORY_VIEW_SIMPLE && flags != RUBY_MEMORY_VIEW_WRITABLE) return false;
 
     struct ptr_data *data = fiddle_ptr_check_memory_view(obj);
     rb_memory_view_init_as_byte_array(view, obj, data->ptr, data->size, false);
